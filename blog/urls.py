4e5259d5f822d741d217ca790ce1c037558734e0
Ken Phetmany <kenphetmanydev@gmail.com>
2022-05-25 18:01:49 +1000

    Added forms and templates for Blog app

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+'''
+    Registers any URLs defined there
+'''
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.blog_index, name="blog_index"),
+    path("<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("<category>/", views.blog_category, name="blog_category"),
+]
