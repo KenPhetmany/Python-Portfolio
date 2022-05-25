ce01da1628925ec2948ef25a8e9e6ca2983381fc
Ken Phetmany <kenphetmanydev@gmail.com>
2022-05-25 16:42:27 +1000

    added new app, portfolio, with models, urls and views

---
+++ projects/views.py
@@ -0,0 +1,23 @@
+import re
+from django.shortcuts import render
+from projects.models import Project
+
+
+def project_index(request):
+    # Get all objects from the project database
+    projects = Project.objects.all()
+    # Dictionary context to send information to the template
+    context = {
+        'projects': projects
+    }
+    # Any entries in the context dictionary is available in the template
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    # PK is the primary key to find the project.
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
