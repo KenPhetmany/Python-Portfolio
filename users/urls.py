0de640b7852d5b9d6acdc6d28f5688c71afeda7d
Ken Phetmany <kenphetmanydev@gmail.com>
2022-06-10 11:20:37 +1000

    Updated Urls to add regristation

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
-    path(r"^dashboard/", views.dashboard, name="dashboard"),
+    path(r"dashboard/", views.dashboard, name="dashboard"),
+    path(r"register/", views.register, name="register"),
+
 ]
